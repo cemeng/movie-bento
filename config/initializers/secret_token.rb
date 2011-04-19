111ca7066a9baa25e7f8858d65603e65bbf7fd9f
Felix Tjandrawibawa <cemeng@gmail.com>
2011-04-19 05:50:21 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoDepot::Application.config.secret_token = '717a2b295060755c9dfb0f46e903f84882ea5e81ac35c41d71e415393f4a28a2b1628f44a491ff439a7ebff7f57059536b10054a1c37f781761710f53eeb185e'
